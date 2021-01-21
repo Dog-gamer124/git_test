cbaf2a570c96ce186b09b0bf7173048b12ce3b68
- <saumitraverma04@gmail.com>
2021-01-21 19:23:44 +0530

    XD

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ file = open("lol.txt", "a")
 n=0
 try:
     while True:
-        file.write("")
+        file.write("a")
         file.close()
         file = open("lol.txt", "a")
         os.system("git add .")
+        #time.sleep(1)
         os.system("git commit -m \"XD\"")
         n+=1
 except KeyboardInterrupt:
