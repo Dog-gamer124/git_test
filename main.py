7b6a0db038e209a731490c3de69f93a61820601c
- <saumitraverma04@gmail.com>
2021-01-21 19:00:56 +0530

    lol

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+file = open("lol.txt", "r+")
+n=0
+
+while True:
+    file.write("lol")
+    os.system("git add .")
+    os.system("git commit -m \"lol\"")
+    file.write("XD")
+    os.system("git add .")
+    os.system("git commit -m \"XD\"")
+    n+=1
+
+
+file.close()
