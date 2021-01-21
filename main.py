92c9ede4694a85fcfd747e7e99545983df1808ef
- <saumitraverma04@gmail.com>
2021-01-21 19:16:25 +0530

    XD

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import time
 
 file = open("lol.txt", "a")
 n=0
+try:
+    while True:
+        file.write("lol")
+        file.close()
+        file = open("lol.txt", "a")
+        os.system("git add .")
+        #time.sleep(1)
+        os.system("git commit -m \"XD\"")
+        n+=1
+except KeyboardInterrupt:
+    print(n)
+    
 
-while True:
-    file.write("lol")
-    file.close()
-    file = open("lol.txt", "a")
-    os.system("git add .")
-    #time.sleep(1)
-    os.system("git commit -m \"XD\"")
-    n+=1
 
-print(n)
 file.close()
