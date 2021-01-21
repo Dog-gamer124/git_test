6e4271753d0e6f73e3bb5c89f95b7326d64fcc1f
- <saumitraverma04@gmail.com>
2021-01-21 19:04:13 +0530

    lol

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ file = open("lol.txt", "r+")
 while True:
     file.write("lol")
     os.system("git add .")
+    file.close()
     os.system("git commit -m \"lol\"")
     file.write("XD")
+    file = open("lol.txt", "r+")
     os.system("git add .")
     os.system("git commit -m \"XD\"")
 
