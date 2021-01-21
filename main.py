0b96ef7491d52d4ce241a5eb8622d81721ac4a15
- <saumitraverma04@gmail.com>
2021-01-21 19:08:52 +0530

    XD

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ n=0
 while True:
     file.write("lol")
     os.system("git add .")
-    os.system("git commit -m \"lol\"" )
+    #os.system("git commit -m \"lol\"" )
     file.write("XD")
     os.system("git add .")
     os.system("git commit -m \"XD\"")
