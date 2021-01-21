a8f9d50efb84b6cad3e709355b2bdb6277240531
- <saumitraverma04@gmail.com>
2021-01-21 19:13:39 +0530

    XD

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ n=0
 
 while True:
     file.write("lol")
+    file.close()
+    file = open("lol.txt", "a")
     os.system("git add .")
     time.sleep(1)
-    #os.system("git commit -m \"lol\"" )
-    file.write("XD")
-    os.system("git add .")
     os.system("git commit -m \"XD\"")
     n+=1
 
-
+print(n)
 file.close()
